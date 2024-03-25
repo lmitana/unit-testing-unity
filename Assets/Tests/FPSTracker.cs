21226060e3e6bad139726a8e45788d0aadde4d16
Lubomir Mitana <lubom@outlook.de>
2024-03-25 12:51:31 +0100

    Play mode tests

---
+++ Assets/Tests/FPSTracker.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSTracker : MonoBehaviour {
+
+    float responseTimeInMilliseconds = 0f;
+    float timePassed = 0f;
+    public float StartupDelayInSeconds = 5f;
+    public float? MinimumFPS = null;
+	
+	void Update () {
+        responseTimeInMilliseconds = Time.unscaledDeltaTime * 1000f;
+        if (timePassed < StartupDelayInSeconds)
+            timePassed += Time.unscaledDeltaTime;
+	}
+
+    private void OnGUI()
+    {
+        if (timePassed < StartupDelayInSeconds)
+            return; // Do nothing until the startup delay passes.
+
+        var style = new GUIStyle();
+        var rect = new Rect(0, 0, Screen.width, Screen.height / 10);
+        style.alignment = TextAnchor.UpperLeft;
+        style.fontSize = Screen.height / 10;
+        style.normal.textColor = Color.gray;
+        var fps = 1f / (responseTimeInMilliseconds / 1000f);
+        if (MinimumFPS.HasValue && fps < MinimumFPS.Value)
+            Debug.LogErrorFormat("FPS ({0}) is below the minimum threshold of {1}!", fps, MinimumFPS.Value);
+        var text = string.Format("{0:0.000} ms ({1:0.000} fps)", responseTimeInMilliseconds, fps);
+        GUI.Label(rect, text, style);
+    }
+}
---
+++ Assets/Tests/PlayModeTests.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+using UnityEngine.AI;
+
+public class PlayModeTests {
+
+    [UnityTest]
+    public IEnumerator HeroPrefabTest() {
+        var source = new GameObject();
+        source.AddComponent<Light>();
+        var camera = source.AddComponent<Camera>();
+        camera.clearFlags = CameraClearFlags.SolidColor;
+        camera.backgroundColor = Color.white;
+        camera.tag = "MainCamera";
+        var asset = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/System/Prefabs/Main Character.prefab");
+        var hero = GameObject.Instantiate(asset, new Vector3(0, -0.7f, 2), new Quaternion(0, 180, 0, 0));
+        var underlyingHero = hero.transform.Find("Hero");
+        var animator = underlyingHero.GetComponent<Animator>();
+        var i = 5;
+        while (i-- > 0)
+        {
+            animator.Play("anm_DwarfHero_Attack01", 0);
+            yield return new WaitForSeconds(1);
+        }
+        GameObject.Destroy(hero);
+        GameObject.Destroy(source);
+    }
+
+    [Test]
+    public void TestSceneLoading()
+    {
+        SceneManager.LoadScene("Level1", LoadSceneMode.Single);
+    }
+
+    [UnityTest]
+    public IEnumerator TestArtificialIntelligenceCombat()
+    {
+        SceneManager.LoadScene("Level1", LoadSceneMode.Single);
+        var hero = (GameObject)null;
+        yield return new WaitUntil(() => (hero = GameObject.Find("Hero")) != null);
+        var navMeshAgent = hero.gameObject.GetComponent<NavMeshAgent>();
+        navMeshAgent.destination = new Vector3(-19.5f, 0, -2.3f);
+        yield return new WaitForSeconds(20);
+        Assert.AreEqual(null, GameObject.Find("Hero"));
+    }
+
+    GameObject fpsTracker;
+
+    [UnityTest]
+    public IEnumerator TestFramesPerSecondDuringArtificialIntelligenceCombat()
+    {
+        SceneManager.sceneLoaded += SceneManager_sceneLoaded;
+        SceneManager.LoadScene("Level1", LoadSceneMode.Single);
+        yield return new WaitForSeconds(10);
+        GameObject.Destroy(fpsTracker);
+        SceneManager.sceneLoaded -= SceneManager_sceneLoaded;
+    }
+
+    private void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1)
+    {
+        var hero = GameObject.Find("Hero");
+        var navMeshAgent = hero.gameObject.GetComponent<NavMeshAgent>();
+        navMeshAgent.destination = new Vector3(-19.5f, 0, -2.3f);
+        fpsTracker = new GameObject();
+        var tracker = fpsTracker.AddComponent<FPSTracker>();
+        tracker.MinimumFPS = 50f;
+    }
+}
